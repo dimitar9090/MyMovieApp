115b1b71d2ea3a56f90eedb1b2ae40c685a31a66
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-09-11 23:25:56 +0300

    Initial Commit

---
+++ MyMovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovieApp
+//
+//  Created by Dimitar Angelov on 11.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
